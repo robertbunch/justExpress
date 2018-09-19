7bcf92a84e1191feab5c5a1162029f3caf84f1e7
Robert Bunch <rob@digitalcrafts.com>
2018-09-19 10:15:03 -0400

    http headers

--- myNewExpressSite/routes/index.js
+++ myNewExpressSite/routes/index.js
@@ -3,6 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // const date = new Date(1969,6,20);
+  // res.set('Date',date)
+  // res.set('Cache-Control','no-store')
+  // res.set('Content-Type','text/html')
+  // res.type('text/html')
+  // fresh and stale
+  // fresh returns true if it's not stale
+  // console.log(req.fresh)
+  // console.log(req.stale)
+  console.log(req.accepts(['json','html']))
   res.render('index', { title: 'Express' });
 });
 
