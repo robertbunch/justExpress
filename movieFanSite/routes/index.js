479f6b3651761d20f40974105e83465ba9d2b1d5
Robert Bunch <rob@digitalcrafts.com>
2018-09-21 11:32:48 -0400

    Add request and view

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+router.use((req, res, next)=>{
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next()
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {  });
+  // request.get takes 2 args:
+  // 1. it takes the URL to http "get"
+  // 2. the callback to run when the http response is back. 3 args:
+  //   1. error (if any)
+  //   2. http response
+  //   3. json/data the server sent back
+  request.get(nowPlayingUrl,(error, response, movieData)=>{
+    // console.log("========The error========")
+    // console.log(error)
+    // console.log("========The response========")
+    // console.log(response)
+    // console.log(movieData)
+    const parsedData = JSON.parse(movieData)
+    // res.json(parsedData)
+    res.render('index',{
+      parsedData: parsedData.results,
+    })
+  })
 });
 
 module.exports = router;
