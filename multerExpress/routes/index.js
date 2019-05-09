b103696df3fcd081a5fd1372f28327c622d17072
Robert Bunch <rdbunch@gmail.com>
2019-05-09 11:55:56 -0400

    Add multer express

---
+++ multerExpress/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+const upload = multer({dest:"public/images/uploads"})
+const fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/formsub',upload.single('meme'),(req, res, next)=>{
+  // console.log(req.file);
+  const newPath = `public/images/uploads/${req.file.originalname}${Date.now()}`;
+  fs.rename(req.file.path, newPath, (err)=>{
+    if(err) throw err;
+    // upload newPath to the db
+    res.json("file uploaded!")
+  });
+  
+})
+
+router.post('/formsubarray',upload.array('meme'),(req, res, next)=>{
+  console.log(req.files);
+  const newPath = `public/images/uploads/${req.file.originalname}${Date.now()}`;
+  fs.rename(req.file.path, newPath, (err)=>{
+    if(err) throw err;
+    // upload newPath to the db
+    res.json("file uploaded!")
+  });
+})
+
+module.exports = router;
