feb0a3f5de700e02d219a8b96ed209e074fade02
Robert Bunch <rdbunch@gmail.com>
2023-05-22 14:33:08 -0400

    April 23 refresh challenge

---
+++ monthlyChallenges/apr23-refresh/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('*',(req,res,next)=>{
+  const { jwtToken } = req.body;
+  // const jwtTokenData = jwt.verify(jwtToken,secret);
+  if(!jwtToken){
+    res.locals.noToken = true;
+  }
+  const jwtTokenData = "";
+  res.locals.jwtToken = jwtTokenData
+  console.log("Testing app level middleware!!!")
+  next() // must call next, to get to the next peice of middleware
+})
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
